4c0249033c604c712c80a1d62e1643ba7d7dbb3a
Kim Kopowski <kim.kopowski@gmail.com>
2019-06-02 07:29:41 -0700

    use local css

--- index.py
+++ index.py
@@ -40,12 +40,6 @@ app.layout = html.Div(
         # Tab content
         html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
 
-        # html.Link(href="https://use.fontawesome.com/releases/v5.2.0/css/all.css", rel="stylesheet"),
-        # html.Link(href="https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css", rel="stylesheet"),
-        # html.Link(href="https://fonts.googleapis.com/css?family=Dosis", rel="stylesheet"),
-        # html.Link(href="https://fonts.googleapis.com/css?family=Open+Sans", rel="stylesheet"),
-        # html.Link(href="https://fonts.googleapis.com/css?family=Ubuntu", rel="stylesheet"),
-        # html.Link(href="https://cdn.rawgit.com/amadoukane96/8a8cfdac5d2cecad866952c52a70a50e/raw/cd5a9bf0b30856f4fc7e3812162c74bfc0ebe011/dash_crm.css", rel="stylesheet")
     ],
     className="row",
     style={"margin": "0%"},
