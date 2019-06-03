46c88b60a9901e58c0325e49fbe4c38a536346fe
Kim Kopowski <kim.kopowski@gmail.com>
2019-06-03 07:40:14 -0700

    add frequency drop down, move chart and add second axis

--- apps/table.py
+++ apps/table.py
@@ -6,6 +6,7 @@ import dash_html_components as html
 import pandas as pd
 
 from app import app
+import apps.utils as utils
 from turtle_manager import Turtle_Manager
 
 df = Turtle_Manager().get_turtles()
@@ -29,10 +30,18 @@ def table():
 
 
 layout = [
+    # top controls
+    html.Div(
+        [
+            utils.drpdwn_frequency("dwn_freq"),
+        ],
+        className="row",
+        style={"marginBottom": "10"},
+    ),
+
+    html.Div(id='datatable-interactivity-container'),
+
     table(),
-
-    html.Div(id='datatable-interactivity-container')
-
 ]
 
 
@@ -40,31 +49,38 @@ layout = [
     Output('datatable-interactivity-container', 'children'),
     [Input('table_1', "derived_virtual_data"),
      Input('table_1', "derived_virtual_selected_rows"),
-     Input('table_1', "ID")])
-def update_bar1(data, selected_rows, ID):
+     Input('table_1', "ID"),
+     Input('dwn_freq', 'value')])
+def update_bar1(data, selected_rows, ID, frequency):
     captureCount = df.set_index('Date')['ID']
-    captureCount = captureCount.groupby(pd.Grouper(freq='Q')).count()
+    captureCount = captureCount.groupby(pd.Grouper(freq=frequency)).count()
     captureCount = captureCount[captureCount > 0]
     box1 = go.Bar(
         x=captureCount.index,
         y=captureCount.values,
-        name='Captures'
+        name='Captures',
+        opacity=0.5,
     )
 
     dateCount = pd.DataFrame(df.Date.unique())
     dateCount['ID'] = '1'
     dateCount = dateCount.set_index(0)['ID']
-    dateCount = dateCount.groupby(pd.Grouper(freq='Q')).count()
+    dateCount = dateCount.groupby(pd.Grouper(freq=frequency)).count()
     dateCount = dateCount[dateCount > 0]
     box2 = go.Bar(
         x=dateCount.index,
         y=dateCount.values,
-        name='Capture Days'
+        yaxis='y2',
+        name='Capture Days',
+        opacity=0.5,
     )
 
     data = [box1, box2]
     layout = go.Layout(
         barmode='group',
+        xaxis={'type': 'category'},
+        yaxis2={'overlaying': 'y',
+                'side': 'right'}
     )
     graph = dcc.Graph(
         id='bar_1',
