6ac9bfdfa6370afa19aaa5cdb4772e6be673435a
Kim Kopowski <kim.kopowski@gmail.com>
2019-06-29 11:34:31 -0700

    Change explore_by_month tests after refactoring

--- tests/test_explore_by_month.py
+++ tests/test_explore_by_month.py
@@ -1,6 +1,10 @@
 from dash_html_components.Div import Div
+from dash_core_components.Graph import Graph
 
 import apps.explore_by_month as page
+from turtle_manager import Turtle_Manager as tm
+
+TURTLES = tm(test=True)
 
 
 def test_layout():
@@ -8,3 +12,13 @@ def test_layout():
     Does my layout render as a Div?
     """
     assert isinstance(page.layout[0], Div)
+
+
+def test_by_month_plot():
+    """
+    Do I render the bar plot?
+    """
+    df = TURTLES.get_df()
+    df, years, caption = page.by_month_chart_format_data(df)
+    by_moth_plot = page.by_month_plot(df, years, caption, 'Q')
+    assert isinstance(by_moth_plot, Graph)
